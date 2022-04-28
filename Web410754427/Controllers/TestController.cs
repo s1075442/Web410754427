073fbef55d0dbf443f632b203eda71b0268e0c61
廖偉成 <s1075442@gm.pu.edu.tw>
2022-04-28 19:56:28 +0800

    textArea

---
+++ Web410754427/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web410754427.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult HW1() {
+            return View();
+        }
+    }
+}
