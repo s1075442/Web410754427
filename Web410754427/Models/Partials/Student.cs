0b132b9d77b8557012678c7454f274d7b35fc2f9
廖偉成 <s1075442@gm.pu.edu.tw>
2022-06-09 20:34:18 +0800

    0609class

--- Web410754427/Models/Partials/Student.cs
+++ Web410754427/Models/Partials/Student.cs
@@ -18,7 +18,7 @@ namespace Web410754427.Models
         [Required(ErrorMessage = "請填寫姓名")]
         [Display(Name="姓名")]
         [StringLength(5,MinimumLength =2,ErrorMessage ="請輸入2~5個字")]
-        public string Name { get; set; }
+        public string Username { get; set; }
 
         //<---------------------------------------->
         [Required(ErrorMessage = "請填寫學號")]
@@ -31,5 +31,10 @@ namespace Web410754427.Models
         [Display(Name = "Email")]
         [EmailAddress]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "必填欄位")]
+        [Display(Name = "是否註冊")]
+        
+        public bool isActive { get; set; }
     }
 }
--- Web410754427/Models/Student.cs
+++ Web410754427/Models/Student.cs
@@ -15,8 +15,9 @@ namespace Web410754427.Models
     public partial class Student
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Username { get; set; }
         public string Number { get; set; }
         public string Email { get; set; }
+        public bool isActive { get; set; }
     }
 }
